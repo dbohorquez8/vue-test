538e5c3dcd863b8d36ebdb62974cef00fdd5aab3
dbohorquez <dbohorquez8+github@gmail.com>
2016-11-22 21:42:44 -0500

    Initial vue setup

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="pokemon">
+    <h1>{{ name }}</h1>
+    <!-- <img src={{ spriteUrl }} alt=""> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pokemon',
+  data() {
+    return {
+      name: 'Loading...',
+      spriteUrl: '',
+    };
+  },
+  created() {
+    this.$http.get('http://pokeapi.co/api/v2/pokemon/1/', (data) => {
+      this.name = data;
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
